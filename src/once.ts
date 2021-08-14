5503085841fa3ac200e1ff098c69ac64efe38724
Dan Percic <danpercic86@gmail.com>
2021-08-14 20:37:09 +0300

    updates

--- src/once.ts
+++ src/once.ts
@@ -1,13 +1,7 @@
 import once from 'lodash.once'
 
-type Func = (...args: unknown[]) => unknown
-
-export function Once() {
-  return function _once<T extends Func>(_target: Object, _propertyKey: PropertyKey, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> {
-    if (!descriptor?.value && typeof descriptor.value !== 'function') {
-      throw new TypeError(`@Once() decorator can only be applied to methods, not to ${typeof descriptor.value}`);
-    }
-
-    return {...descriptor, value: once<T>(descriptor.value)};
+export function Once(): MethodDecorator {
+  return function _once(_target: Object, _propertyKey: PropertyKey, descriptor: PropertyDescriptor): PropertyDescriptor {
+    return {...descriptor, value: once(descriptor.value)};
   };
 }
